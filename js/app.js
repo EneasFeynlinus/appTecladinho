b67491190c0a84f6bdbcfb88b59164f168455b1d
Eneas Junior <eneas.silva.jr@gmail.com>
2023-06-24 13:19:44 -0300

    feat: Adding the Key class to add eventListener to each one, passing the instructions as a callback when the mousedown or mouseup event is triggered.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const keys = document.querySelectorAll('.key')
+
+const handleMouseDown = (key) => {
+  key.style.background = '#ddd'
+}
+
+const handleMouseUp = (key) => {
+  key.style.background = 'white'
+}
+
+keys.forEach((key) => {
+  key.addEventListener('mousedown', () => handleMouseDown(key))
+  key.addEventListener('mouseup', () => handleMouseUp(key))
+})

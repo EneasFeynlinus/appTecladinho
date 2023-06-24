17f394999c0ee709679dcd95496fa41b48e12708
Eneas Junior <eneas.silva.jr@gmail.com>
2023-06-24 16:14:40 -0300

    feat: Referenced span with class switcher to be manipulated when switchin to see keyboards keys or not.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const keys = document.querySelectorAll('.key')
+const checkBox = document.querySelector('.checkbox_keys')
+const switcher = document.querySelector('.switcher')
 
 const playNote = (note) => {
   const audio = new Audio(`../notes/${note}.wav`)
@@ -26,4 +28,13 @@ const handleMouseUp = (key) => {
 keys.forEach((key) => {
   key.addEventListener('mousedown', () => handleMouseDown(key))
   key.addEventListener('mouseup', () => handleMouseUp(key))
+})
+
+checkBox.addEventListener('change', ({ target }) => {
+  if (target.checked) {
+    switcher.classList.add('switcher--active')
+    return
+  }
+
+  switcher.classList.remove('switcher--active')
 })
